78ab88464e8cf97d79b204dd42db259fdb0aa8bd
hshoon <hshoon@egiskorea.com>
2025-03-19 08:34:38 +0900

    chap3 : 첫 납부일과 만료일자 다를 때 만원 납부

--- TDD_maven/src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ TDD_maven/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -36,7 +36,7 @@ public class ExpiryDateCalculatorTest {
     }
 
     @Test
-    void 납부일과_한달_뒤_일자가_같지_않음 () {
+    void 납부일과_한달_뒤_일자가_같지_않음() {
 //        assertExpiryDate(LocalDate.of(2019, 1, 31), 10_000, LocalDate.of(2019, 2, 28));
 //        assertExpiryDate(LocalDate.of(2019, 5, 31), 10_000, LocalDate.of(2019, 6, 30));
 //        assertExpiryDate(LocalDate.of(2020, 1, 31), 10_000, LocalDate.of(2020, 2, 29));
@@ -61,6 +61,31 @@ public class ExpiryDateCalculatorTest {
                         .build(),
                 LocalDate.of(2020, 2, 29)
         );
+    }
+
+    @Test
+    void 첫_납부일과_만료일_일자가_다를때_만원_납부() {
+        PayData payData = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 1, 31))
+                .billingDate(LocalDate.of(2019, 2, 28))
+                .payAmount(10_000)
+                .build();
+
+        PayData payData2 = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 1, 30))
+                .billingDate(LocalDate.of(2019, 2, 28))
+                .payAmount(10_000)
+                .build();
+
+        PayData payData3 = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 5, 31))
+                .billingDate(LocalDate.of(2019, 6, 30))
+                .payAmount(10_000)
+                .build();
+
+        assertExpiryDate(payData, LocalDate.of(2019, 3, 31));
+        assertExpiryDate(payData2, LocalDate.of(2019, 3, 30));
+        assertExpiryDate(payData3, LocalDate.of(2019, 7, 31));
     }
 
     private void assertExpiryDate(PayData payData, LocalDate expiryExpiryDate) {
